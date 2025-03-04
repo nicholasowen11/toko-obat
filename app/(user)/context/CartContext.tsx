1babd7f9ef4b83b2914f47927bc88de3cc0bc1d5
Owen Nich 11 <nicholas.sentosa@binus.ac.id>
2025-03-04 23:23:09 +0700

    Fixed Errors Deployment #6

--- app/(user)/context/CartContext.tsx
+++ app/(user)/context/CartContext.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 // import { createContext } from "react";
-import { CartItem, Product } from "@/types";
+// import { CartItem, Product } from "@/types";
 
 // interface CartContextType {
 //   cart: CartItem[];
